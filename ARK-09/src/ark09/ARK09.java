445b4d659eb1e26befdaf82cf80fb3447078c5f0
Victor Augusto <79682345+VictorAugustoRodriguesGomes@users.noreply.github.com>
2023-01-18 00:14:05 -0300

    base

--- ARK-09/src/ark09/ARK09.java
+++ ARK-09/src/ark09/ARK09.java
@@ -1,80 +1,67 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ark09;
 
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author Anigos2
- */
 public class ARK09 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         int soma;
-        int mutiplicaÃ§Ã£o;
+        int mutiplicação;
         int numero1;
-        numero1 = Integer.parseInt(JOptionPane.showInputDialog("Digite um numero 1"));
+        
+        numero1 = Integer.parseInt(JOptionPane.showInputDialog("Informe um número: "));
         int numero2;
-        numero2 = Integer.parseInt(JOptionPane.showInputDialog("Digite um numero 2"));
+        numero2 = Integer.parseInt(JOptionPane.showInputDialog("Informe um segundo número: "));
         int numero3;
-        numero3 = Integer.parseInt(JOptionPane.showInputDialog("Digite um numero 3"));
+        numero3 = Integer.parseInt(JOptionPane.showInputDialog("Informe um terceiro número: "));
         if (numero1 >= numero2 && numero2 >= numero3) {
             soma = numero1 + numero2;
-            System.out.println("A soma dos dois maiores Ã©:" + soma);
+            System.out.println("A soma dos dois maiores é: " + soma);
         }
         if (numero1 >= numero3 && numero3 >= numero2) {
             soma = numero1 + numero3;
-            System.out.println("A soma dos dois maiores Ã©:" + soma);
+            System.out.println("A soma dos dois maiores é: " + soma);
         }
         if (numero2 >= numero1 && numero1 >= numero3) {
             soma = numero2 + numero3;
-            System.out.println("A soma dos dois maiores Ã©:" + soma);
+            System.out.println("A soma dos dois maiores é: " + soma);
         }
         if (numero3 >= numero2 && numero2 >= numero1) {
             soma = numero2 + numero3;
-            System.out.println("A soma dos dois maiores Ã©:" + soma);
+            System.out.println("A soma dos dois maiores é: " + soma);
         }
         if (numero3 >= numero1 && numero1 >= numero3) {
             soma = numero3 + numero1;
-            System.out.println("A soma dos dois maiores Ã©:" + soma);
+            System.out.println("A soma dos dois maiores é: " + soma);
         }
         if (numero2 >= numero3 && numero3 >= numero1) {
             soma = numero2 + numero3;
-            System.out.println("A soma dos dois maiores Ã©:" + soma);
+            System.out.println("A soma dos dois maiores é: " + soma);
         }
 
         if (numero1 <= numero2 && numero2 <= numero3) {
-            mutiplicaÃ§Ã£o = numero1 * numero2;
-            System.out.println("A mutiplicaÃ§Ã£o dos dois numeros menor Ã©:" + mutiplicaÃ§Ã£o);
+            mutiplicação = numero1 * numero2;
+            System.out.println("A mutiplicação dos dois numeros menor é: " + mutiplicação);
         }
         if (numero1 <= numero3 && numero3 <= numero2) {
-            mutiplicaÃ§Ã£o = numero1 * numero3;
-            System.out.println("A mutiplicaÃ§Ã£o dos dois numeros menor Ã©:" + mutiplicaÃ§Ã£o);
+            mutiplicação = numero1 * numero3;
+            System.out.println("A mutiplicação dos dois numeros menor é: " + mutiplicação);
         }
         if (numero2 <= numero1 && numero1 <= numero3) {
-            mutiplicaÃ§Ã£o = numero2 * numero1;
-            System.out.println("A mutiplicaÃ§Ã£o dos dois numeros menor Ã©:" + mutiplicaÃ§Ã£o);
+            mutiplicação = numero2 * numero1;
+            System.out.println("A mutiplicação dos dois numeros menor é: " + mutiplicação);
         }
         if (numero2 <= numero3 && numero3 <= numero1) {
-            mutiplicaÃ§Ã£o = numero2 * numero3;
-            System.out.println("A mutiplicaÃ§Ã£o dos dois numeros menor Ã©:" + mutiplicaÃ§Ã£o);
+            mutiplicação = numero2 * numero3;
+            System.out.println("A mutiplicação dos dois numeros menor é: " + mutiplicação);
         }
         if (numero3 <= numero1 && numero1 <= numero2) {
-            mutiplicaÃ§Ã£o = numero3 * numero1;
-            System.out.println("A mutiplicaÃ§Ã£o dos dois numeros menor Ã©:" + mutiplicaÃ§Ã£o);
+            mutiplicação = numero3 * numero1;
+            System.out.println("A mutiplicação dos dois numeros menor é: " + mutiplicação);
         }
         if (numero3 <= numero2 && numero2 <= numero1) {
-            mutiplicaÃ§Ã£o = numero3 * numero2;
-            System.out.println("A mutiplicaÃ§Ã£o dos dois numeros menor Ã©:" + mutiplicaÃ§Ã£o);
+            mutiplicação = numero3 * numero2;
+            System.out.println("A mutiplicação dos dois numeros menor é: " + mutiplicação);
         }
-
     }
-
 }
